a32d45ac46b9fbb37d204276d4d61d35d830789a
Kibeh <kibetimmanuel0@gmail.com>
2024-06-06 13:37:49 +0300

    feat: Add footer component
    - Create a new Footer component
    - Implement the footer structure and content

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,29 @@
+import { NavLink } from "react-router-dom"
+
 function Footer() {
   return (
-    <div>
-      Footer
-    </div>
+    <>
+      {/* create the footer for this application */}
+      <footer className='bg-accent py-8 px-4 mt-4'>
+        <div className='container mx-auto flex justify-between items-center'>
+          <div>
+            <h1 className='text-2xl font-bold'>Snagify</h1>
+            <p className='mt-1'>
+              © {new Date().getFullYear()} All rights reserved.
+            </p>
+          </div>
+          <div>
+            <NavLink to='/terms'  className='hover:underline underline-offset-4'>
+              Terms of Service
+            </NavLink>
+            <span className='mx-2'>|</span>
+            <NavLink to='/privacy' className='hover:underline underline-offset-4'>
+              Privacy Policy
+            </NavLink>
+          </div>
+        </div>
+      </footer>
+    </>
   )
 }
 
